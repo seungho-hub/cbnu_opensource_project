7aae7d386187f312f182d4d24d00abb5b8b534d1
seungho-hub <kmc54320@gmail.com>
2022-12-11 17:03:00 +0900

    [ADD] : cancle, participant, close button to Product Detail Modal

--- server/src/main/resources/views/src/home/Components/Modal/ProductDetailModal.svelte
+++ server/src/main/resources/views/src/home/Components/Modal/ProductDetailModal.svelte
@@ -1,15 +1,106 @@
 <script>
   import { scale } from "svelte/transition";
-  export let showProductDetailModal;
+  import { auth } from "../../../stores/auth";
+  import { products } from "../../../stores/products";
+  let user;
+  auth.subscribe((auth) => {
+    user = auth.user;
+  });
+
+  export let bindVar;
+
+  function closeProduct(code) {
+    products.update((items) => {
+      items = items.filter(function (item) {
+        return item.code !== code;
+      });
+
+      bindVar.showProductDetailModal = false;
+      return items;
+    });
+  }
+
+  function cancleParticipant(code) {
+    products.update((items) => {
+      let item = items.find(function (item) {
+        return item.code == code;
+      });
+
+      item.participants = item.participants.filter(
+        (participant) => participant != user.username
+      );
+    });
+  }
 </script>
 
 <div
   class="modal-bg"
   on:click|self={() => {
-    showProductDetailModal = !showProductDetailModal;
+    bindVar.showProductDetailModal = !bindVar.showProductDetailModal;
   }}
 >
-  <modal id="product-detail-modal" transition:scale> sd </modal>
+  <modal id="product-detail-modal" transition:scale>
+    <div class="top">
+      <div class="img-wrapper">
+        <img src={bindVar.targetProduct.img} alt="" />
+      </div>
+      <div class="info">
+        <div class="label-code">
+          <div class="label">
+            <p>{bindVar.targetProduct.name}</p>
+          </div>
+          <div class="code">
+            <p>{bindVar.targetProduct.code}</p>
+          </div>
+        </div>
+
+        <div class="link">
+          <a href={bindVar.targetProduct.url}>상품 링크</a>
+        </div>
+        <div class="price">
+          <span class="text">(1인)</span>
+          &nbsp
+          <span class="divided-price"
+            >{bindVar.targetProduct.divided_price}</span
+          >
+          &nbsp
+          <p class="original-price">{bindVar.targetProduct.price}원</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="mid">
+      <div class="description">
+        {bindVar.targetProduct.description}
+      </div>
+      <div class="participants">
+        {#each bindVar.targetProduct.participants as participant}
+          <div class="participant">
+            <span>{participant}</span>
+          </div>
+        {/each}
+      </div>
+    </div>
+
+    <div class="buttons">
+      {#if bindVar.targetProduct.participants.includes(user.username)}
+        <button
+          class="cancle"
+          on:click={cancleParticipant(bindVar.targetProduct.code)}
+          >참여 취소</button
+        >
+      {:else if bindVar.targetProduct.register == user.username}
+        <button
+          class="close"
+          on:click={() => {
+            closeProduct(bindVar.targetProduct.code);
+          }}>마감하기</button
+        >
+      {:else}
+        <button class="participant">참여하기</button>
+      {/if}
+    </div>
+  </modal>
 </div>
 
 <style lang="scss">
@@ -21,5 +112,111 @@
     width: 40%;
     background-color: var(--bg-more-light-color);
     padding: 2em;
+<<<<<<< Updated upstream
+=======
+
+    .top {
+      display: grid;
+      grid-template-areas: "img" "info";
+      gap: 1em;
+      .img-wrapper {
+        grid-area: img;
+        width: $product-image-width;
+        height: $product-image-width;
+        img {
+          width: $product-image-width;
+          height: $product-image-width;
+        }
+      }
+      .info {
+        grid-area: info;
+        display: grid;
+        grid-template-areas: "label-code" "link" "price";
+
+        .label-code {
+          grid-area: label-code;
+          .label {
+            display: inline;
+            p {
+              font-size: 1.6em;
+            }
+          }
+          .code {
+            display: inline;
+            p {
+              font-size: 1em;
+              color: var(--sig);
+            }
+          }
+        }
+
+        .link {
+          margin-top: 1em;
+          width: $product-image-width;
+          grid-area: link;
+          text-overflow: ellipsis;
+        }
+
+        .price {
+          grid-area: price;
+          display: flex;
+          align-items: flex-end;
+          justify-content: end;
+          .divided_price {
+            color: var(--font-color);
+          }
+          .original-price {
+            text-align: end;
+            color: rgba(121, 121, 121, 0.5);
+          }
+        }
+      }
+    }
+    .mid {
+      padding: 0 1em;
+      margin-top: 1.5em;
+      .description {
+        margin-bottom: 3em;
+      }
+      .participants {
+        display: flex;
+        gap: 1em;
+        .participant {
+          background-color: var(--sig);
+          span {
+            padding: 0.3em 0.5em;
+            background-color: var(--sig);
+            color: var(--font-dark-color);
+          }
+
+          &:hover {
+            cursor: pointer;
+          }
+        }
+      }
+    }
+
+    .buttons {
+      margin-top: 4em;
+      button {
+        width: 100%;
+        border-radius: 0;
+      }
+      .cancle {
+        background-color: #f85d5d;
+        color: var(--font-dark-color);
+      }
+
+      .participant {
+        background-color: var(--sig);
+        color: var(--font-dark-color);
+      }
+
+      .close {
+        background-color: #f57338;
+        color: var(--font-dark-color);
+      }
+    }
+>>>>>>> Stashed changes
   }
 </style>
